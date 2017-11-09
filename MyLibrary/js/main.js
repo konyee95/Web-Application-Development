d0015acc7738189ed3019bda6eaafc70d8090517
Rex Low <qiweilow950823@gmail.com>
2017-11-09 20:37:53 +0800

    update search id names

--- MyLibrary/js/main.js
+++ MyLibrary/js/main.js
@@ -1,21 +1,21 @@
 function onSearchInputFocus() {
-  const searchForm = document.getElementById("searchForm");
-  const searchInput = document.getElementById("searchInput");
+  const searchForm = document.getElementById("search-form");
+  const searchInput = document.getElementById("search-input");
 
   searchForm.classList.add("search-form-expanded");
   searchInput.classList.add("search-input-expanded");
 }
 
 function onSearchInputLoseFocus() {
-  const searchForm = document.getElementById("searchForm");
-  const searchInput = document.getElementById("searchInput");
+  const searchForm = document.getElementById("search-form");
+  const searchInput = document.getElementById("search-input");
 
   searchForm.classList.remove("search-form-expanded")
   searchInput.classList.remove("search-input-expanded")
 }
 
 function init() {
-  const searchInput = document.getElementById("searchInput");
+  const searchInput = document.getElementById("search-input");
 
   searchInput.addEventListener("focus", onSearchInputFocus);
   searchInput.addEventListener("focusout", onSearchInputLoseFocus);
