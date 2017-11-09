27f94ffdf31dc2d724162e4e2a4e8178cbfac37b
Rex Low <qiweilow950823@gmail.com>
2017-11-09 21:02:08 +0800

    added side panel

--- MyLibrary/js/hamburger-menu.js
+++ MyLibrary/js/hamburger-menu.js
@@ -1,3 +1,11 @@
 function hamburgerMenu(x) {
   x.classList.toggle("change");
+
+  if (!x.classList.contains("change")) {
+    document.getElementById("side-panel").style.width = "0";
+    document.body.style.backgroundColor = "white";
+  } else {
+    document.getElementById("side-panel").style.width = "300px";
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  }
 }
