322f3a1e3b37d3492f93ff5b36b1918f21f32279
Rex Low <qiweilow950823@gmail.com>
2017-11-13 12:01:16 +0800

    done with publisher table

--- MyLibrary/sql/publishers.sql
+++ MyLibrary/sql/publishers.sql
@@ -1,10 +1,14 @@
-CREATE TABLE 'publishers'(
-    'publisher_id' varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Book Id',
-    'name' varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Publisher Name'
+CREATE TABLE `publishers`(
+    `publisher_index` int(5) UNSIGNED NOT NULL COMMENT 'Publisher Index',
+    `publisher_id` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Publisher Id',
+    `publisher_name` varchar(200) DEFAULT NULL COMMENT 'Publisher Name'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Publisher table';
 
 TRUNCATE TABLE `publishers`;
 
 ALTER TABLE `publishers`
-  ADD PRIMARY KEY (`publisher_id`),
-  ADD UNIQUE KEY 'publisher_id'('publisher_id'); 
+  ADD PRIMARY KEY (`publisher_index`),
+  ADD UNIQUE KEY `publisher_id` (`publisher_id`); 
+
+ALTER TABLE `publishers`
+  MODIFY `publisher_index` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Publisher Index';
