cc5219b82e11565adfccd42827198fff534c625a
Rex Low <qiweilow950823@gmail.com>
2017-11-13 11:25:18 +0800

    staff table done

--- MyLibrary/sql/staffs.sql
+++ MyLibrary/sql/staffs.sql
@@ -1,13 +1,15 @@
-CREATE TABLE 'staff'(
-    'staff_index' int(7) UNSIGNED NOT NULL COMMENT 'Book Record Index',
-    'staff_id' varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Book Id',
-    'password' varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Student password',
-    'name' varchar(200)
+CREATE TABLE `staffs`(
+    `staff_index` int(5) UNSIGNED NOT NULL COMMENT 'Staff Index',
+    `staff_id` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Staff Id',
+    `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Staff password',
+    `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time registered'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Staff table';
 
-TRUNCATE TABLE `staff`;
+TRUNCATE TABLE `staffs`;
 
-ALTER TABLE `book_records`
+ALTER TABLE `staffs`
   ADD PRIMARY KEY (`staff_index`),
-  ADD PRIMARY KEY (`staff_id`),
-  ADD UNIQUE KEY 'staff_id'('staff_id');
+  ADD UNIQUE KEY `staff_id`(`staff_id`);
+
+ALTER TABLE `staffs`
+  MODIFY `staff_index` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Staff Index';
