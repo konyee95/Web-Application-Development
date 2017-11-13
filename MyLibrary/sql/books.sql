0c58938286a0668f3e225f41606eef5670631264
Rex Low <qiweilow950823@gmail.com>
2017-11-13 11:42:33 +0800

    Done with books

--- MyLibrary/sql/books.sql
+++ MyLibrary/sql/books.sql
@@ -1,12 +1,19 @@
-CREATE TABLE 'books'(
-    'book_id' varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Book Id',
-    'title' varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Book Title',
-    'author' varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Author',
-    'description' varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Description',
-    'availability' varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Availability',
+CREATE TABLE `books`(
+    `book_index` int(5) UNSIGNED NOT NULL COMMENT 'Book Index',
+    `book_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Book Id',
+    `title` varchar(1000)  NOT NULL COMMENT 'Book Title',
+    `author` varchar(1000)  NULL COMMENT 'Author',
+    `description` varchar(1000)  NULL COMMENT 'Description',
+    `availability` boolean NOT NULL DEFAULT 1 COMMENT 'Book availability',
+    `image_url` varchar(200) NULL COMMENT 'Image url',
+    `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time added'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Book table';
 
 TRUNCATE TABLE `books`;
 
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`book_index`);
+  ADD PRIMARY KEY (`book_index`),
+  ADD UNIQUE KEY (`book_id`);
+
+ALTER TABLE `books`
+  MODIFY `book_index` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Book Index';
