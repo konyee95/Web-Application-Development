6cbf33a0340695ddb4a63c5c87349d5ede7d960c
Rex Low <qiweilow950823@gmail.com>
2017-11-14 12:50:56 +0800

    added php file for class today

---
+++ rex/php/footer.php
@@ -0,0 +1,6 @@
+<html lang="en">
+<body>
+  <center><h5>This is footer file</h5></center>
+  <hr/>
+</body>
+</html>
---
+++ rex/php/header.php
@@ -0,0 +1,6 @@
+<html lang="en">
+<body>
+  <center><h5>This is header file</h5></center>
+  <hr/>
+</body>
+</html>
