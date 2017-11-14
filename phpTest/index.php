d4c2ba38b6300aee2ada1b369ce08a1493ed57fd
Kon Yee <konyee95@gmail.com>
2017-11-14 12:50:16 +0800

    did first php lab

---
+++ phpTest/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>A Simple PHP: HELLO WORLD</title>
+</head>
+<body>
+<?php
+include_once("header.php");
+#Using comment in code
+ echo "<h2><font color='red' face='Arial'>Hello Students!</font></h2>";
+
+ $name="Chin Kon Yee";
+ $address="bukit beruang,melaka";
+ echo "Name:" .$name. "<p>"."Address:".$address;
+ include_once("footer.php");
+?>
+</body>
+</html>
