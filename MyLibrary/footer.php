42a71aebe57d6781dd8f70005831ef8ff8c45e8b
Rex Low <qiweilow950823@gmail.com>
2017-11-15 00:23:15 +0800

    updated footer

--- MyLibrary/footer.php
+++ MyLibrary/footer.php
@@ -1,11 +1,26 @@
 <div class="footer-container">
-  <div class="footer-logo">
+  <div class="footer-item-container">
+    <div id="footer-item-1" class="footer-item">
+      <a href="#">
+        <h3>MyLibrary Online Portal</h3>
+      </a>
+    </div>
 
+    <div id="footer-item-2" class="footer-item">
+      <h3>Directories</h3>
+      <a href="">Home</a>
+      <a href="">About Us</a>
+      <a href="">Explore</a>
+      <a href="">Profile</a>
+    </div>
+
+    <div id="footer-item-3" class="footer-item">
+      <h3>UTeM</h3>
+      <h4>Pejabat Perpustakaan, Universiti Teknikal Malaysia Melaka, Hang Tuah Jaya, 76100 Durian Tunggal, Melaka, Malaysia.</h4>
+      <h4>Phone: +606-3316830/6831</h4>
+      <h4>Fax: +606-3316811</h4>
+    </div>
   </div>
-  <div class="footer-content">
-    <h3>MyLibrary Online Portal</h3>
-    <p>Pejabat Perpustakaan, Universiti Teknikal Malaysia Melaka, Hang Tuah Jaya, 76100 Durian Tunggal, Melaka, Malaysia.</p>
-    <p>Phone: +606-3316830/6831</p>
-    <p>Fax: +606-3316811</p>
-  </div>
+
+  <p class="copyright-text">MyLibrary © 2017</p>
 </div>
