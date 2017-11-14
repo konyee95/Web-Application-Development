829754d81e7e12999df312079c2240aa16405de1
Rex Low <qiweilow950823@gmail.com>
2017-11-15 00:28:56 +0800

    fixed a bug where entering from root directory will not activate slider

--- MyLibrary/js/main.js
+++ MyLibrary/js/main.js
@@ -234,7 +234,7 @@ const init = () => {
   window.addEventListener("click", sidePanelListener);
 
   /* only activate slider at homepage */
-  if (checkPathName() === "index.php") {
+  if (checkPathName() === "index.php" || "MyLibrary") {
     imageSlider(9);
   }
 }
