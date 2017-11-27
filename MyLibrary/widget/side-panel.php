17b72ac8cb2ca099ae9df3bd86af2afdf384de55
Rex Low <qiweilow950823@gmail.com>
2017-11-27 21:21:17 +0800

    updated side panel

--- MyLibrary/widget/side-panel.php
+++ MyLibrary/widget/side-panel.php
@@ -1,14 +1,28 @@
 <!DOCTYPE html>
 <?php
-    getSidePanelUsername();
+    if (isset($_SESSION['student_index'])) {
+      $student = new Student($_SESSION['student_index']);
+    }
 ?>
 
-<div id="side-panel" class="side-panel">
-  <div id="user-account-header" class="user-account-header">
-    <p><?php echo __NAME__; ?></p>
+  <div id="side-panel" class="side-panel">
+    <div id="user-account-header" class="user-account-header">
+      <?php
+      if (isset($_SESSION['student_index'])) {
+        echo "<p>$student->student_name</p>";
+      } else {
+        echo "<p>User Account</p>";
+      }
+    ?>
+    </div>
+    <div id="user-account-container" class="user-account-container">
+      <?php
+    if (isset($_SESSION['student_index'])) {
+      echo "<p>Welcome back!</p>";
+    } else {
+      echo "<p>Log in your account to save your favourite books and read later!</p>";
+      echo "<a class='button button-primary' href='javascript:dynamicPath();'>Login</a>";
+    }
+  ?>
+    </div>
   </div>
-  <div id="user-account-container" class="user-account-container">
-    <p>Log in your account to save your favourite books and read later!</p>
-    <a class="button button-primary" href="javascript:dynamicPath();">Login</a>
-  </div>
-</div>
