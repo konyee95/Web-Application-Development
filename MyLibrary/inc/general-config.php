0548fe98155e3a3bf59ce0ab6d47a7f5d01204c2
Rex Low <qiweilow950823@gmail.com>
2017-12-01 23:55:20 +0800

    added css for book

--- MyLibrary/inc/general-config.php
+++ MyLibrary/inc/general-config.php
@@ -14,6 +14,7 @@
 <link rel="stylesheet" type="text/css" href="./css/navigation-bar.css" />
 <link rel="stylesheet" type="text/css" href="./css/profile.css" />
 <link rel="stylesheet" type="text/css" href="./css/footer.css" />
+<link rel="stylesheet" type="text/css" href="./css/book.css" />
 
 
 <!-- scripts -->
